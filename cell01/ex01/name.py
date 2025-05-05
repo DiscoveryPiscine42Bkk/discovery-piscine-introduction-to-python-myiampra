9586414de737343216013a9f30094f4bddb3e737
myiampra <66010669@kmitl.ac.th>
2025-05-05 13:28:44 +0700

    submit ex03

---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Mekkapat"
+last_name = "Yiamprasert"
+
+whole_name = first_name + " " + last_name
+print(whole_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,5 @@
+bangkok = 42
+myAge = 19
+
+my_age = bangkok + myAge
+print(my_age)
