9d5d0419b6a919f9cadaf63e492e5d82dc714b3a
myiampra <66010669@kmitl.ac.th>
2025-05-06 15:43:50 +0700

    submit ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+one = int(input("Give me the first number: "))
+two = int(input("Give me the second number: "))
+print("Thank you!")
+i = one + two
+j = one - two
+k = one / two
+l = one * two
+print(one,"+",two,"=",i)
+print(one,"-",two,"=",j)
+print(one,"/",two,"=",int(k))
+print(one,"*",two,"=",l)
