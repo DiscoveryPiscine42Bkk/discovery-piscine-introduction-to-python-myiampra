57911223527d5e4c0a2a0e286e68db3e9bf41dc2
myiampra <66010669@kmitl.ac.th>
2025-05-06 15:25:56 +0700

    submit ex01

---
+++ cell04/ex01/age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+age = int(input("Please tell me your age: "))
+print("You are currently",age ,"years old.")
+i = 10
+while i <= 30:
+    print("In",i,"years, you'll be",age+i,"years old.")
+    i += 10
