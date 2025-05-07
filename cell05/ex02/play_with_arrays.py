da4ac37e725e7ee3330e368f08211686f19481b5
myiampra <66010669@kmitl.ac.th>
2025-05-07 13:22:13 +0700

    array02

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 original = [2, 8, 9, 48, 8, 22, -12 ,2]
-new = [4, 10, 11, 50, 10, 24, -10, 4]
-new.remove(4),new.remove(-10),new.remove(4)
+new = []
+
+for i in original:
+    if i > 5:
+        new.append(i+2)
 print(original)
 print(new)
