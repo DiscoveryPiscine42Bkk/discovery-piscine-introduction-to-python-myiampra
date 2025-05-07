0d8d587e55fb7396906bce2effb11be2fa5e0153
myiampra <66010669@kmitl.ac.th>
2025-05-07 12:30:37 +0700

    array02

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -3,5 +3,5 @@
 original = [2, 8, 9, 48, 8, 22, -12 ,2]
 new = [4, 10, 11, 50, 10, 24, -10, 4]
 new.remove(4),new.remove(-10),new.remove(4)
-print("Original array:", original)
-print("New array:", new)
+print(original)
+print(new)
