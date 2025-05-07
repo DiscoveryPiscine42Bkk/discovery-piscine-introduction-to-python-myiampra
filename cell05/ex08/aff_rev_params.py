dc00d0afe523b757d760aa299f4c176292e891f8
myiampra <66010669@kmitl.ac.th>
2025-05-07 15:16:45 +0700

    para08

--- cell05/ex08/aff_rev_params.py
+++ cell05/ex08/aff_rev_params.py
@@ -2,9 +2,8 @@
 
 import sys
 
-if len(sys.argv) == 4:
-    print(sys.argv[3])
-    print(sys.argv[2])
-    print(sys.argv[1])
-else:
+if len(sys.argv) != 4:
     print("none")
+else:
+    for i in reversed(sys.argv[1:]):
+        print(i)
