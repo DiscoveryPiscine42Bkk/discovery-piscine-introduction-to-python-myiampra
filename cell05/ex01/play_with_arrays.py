9adcd0d829a665d4662554f2eeb6bcb067486a97
myiampra <66010669@kmitl.ac.th>
2025-05-07 13:16:51 +0700

    array01

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 original = [2, 8, 9, 48, 8, 22, -12 ,2]
-new = [4, 10, 11, 50, 10, 24, -10, 4]
+new = []
 
-print("Original array:", original)
-print("New array:", new)
+for i in original:
+    new.append(i+2)
+print(original)
+print(new)
